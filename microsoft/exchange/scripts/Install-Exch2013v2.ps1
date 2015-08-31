52d813ece09ac18e1de362844336f40a3f1388a4
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2015-08-31 10:58:47 -0700

    Updated AMIs

---
+++ microsoft/exchange/scripts/Install-Exch2013v2.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+param(
+    [string]
+    [Parameter(Mandatory=$true, Position=0)]
+    $InstallPath,
+
+    [int]
+    [Parameter(Mandatory=$false, Position=1)]
+    $Server = 1
+)
+try {
+    $InstallPath = Join-Path -Path $InstallPath -ChildPath Setup.exe
+    Invoke-Expression "$InstallPath /mode:Install /role:ClientAccess,Mailbox /MdbName:DefaultDB$Server /DbFilePath:'C:\DefaultDB\DefaultDB$Server\DefaultDB$Server.edb' /LogFolderPath:'C:\DefaultDB\DefaultDB$Server' /InstallWindowsComponents /IAcceptExchangeServerLicenseTerms" -ErrorAction Stop
+}
+catch {
+    $_ | Write-AWSQuickStartException    
+}
